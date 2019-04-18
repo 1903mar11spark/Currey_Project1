6ab87e062aedfad22ceea298a7552fd16aa2aca1
Shawn Currey <shawn.m.currey@gmail.com>
2019-04-18 01:34:44 -0400

    touched up on employee

--- Project1/src/main/java/com/revature/Beans/Employee.java
+++ Project1/src/main/java/com/revature/Beans/Employee.java
@@ -2,14 +2,7 @@ package com.revature.Beans;
 
 public class Employee
 {
-    /*
-    id SERIAL PRIMARY KEY,
-	job_title VARCHAR(100),
-	first_name VARCHAR(50),
-	last_name VARCHAR(50),
-	reports_to INTEGER REFERENCES employees(id)
-    */
-
+    
     int id;
     String jobTitle;
     String firstName, lastName;
